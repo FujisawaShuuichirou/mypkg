05683643b9be8b50161d001290be5e21b7642bb6
koppechan90 <s21C1104DW@s.chibakoudai.jp>
2022-12-30 18:34:27 +0900

    mod listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -4,12 +4,12 @@ from std_msgs.msg import Int16
 
 def cb(msg):
     global node
-    global doub
-    #node.get_logger().info("Listen: %d" % msg.data)
-    doub += msg.data
-    print(doub)
+    #global doub
+    node.get_logger().info("Listen: %d" % msg.data)
+    #doub += msg.data
+    #print(doub)
 
-doub = 0
+#doub = 0
 rclpy.init()
 node = Node("listener")
 pub = node.create_subscription(Int16, "countup", cb, 10)
